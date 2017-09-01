cbe264074832b867066e9deaadfb19cbbd7ebe82
comp <prokopchukmichail@gmail.com>
2017-09-01 17:01:49 +0300

    refactoring

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# itworkstesttask
+# itworks test task
+
+# Data base property and scripts:
+itworkstesttask-rest/src/main/resources/db
+
+# Maven build:
+mvn -install
+
+# Deploy script 
+deploy.bat
---
+++ deploy.bat
@@ -0,0 +1,9 @@
+cd %CATALINA_HOME%\webapps
+DEL %CATALINA_HOME%\webapps\itworkstesttask-rest-1.0-SNAPSHOT.war
+rd %CATALINA_HOME%\webapps\itworkstesttask-rest-1.0-SNAPSHOT /s /q
+DEL %CATALINA_HOME%\webapps\itworkstesttask-web-1.0-SNAPSHOT.war
+rd %CATALINA_HOME%\webapps\itworkstesttask-web-1.0-SNAPSHOT /s /q
+move %CD%\\itworkstesttask-rest\target\itworkstesttask-rest-1.0-SNAPSHOT.war %CATALINA_HOME%\webapps
+move %CD%\\itworkstesttask-web\target\itworkstesttask-web-1.0-SNAPSHOT.war %CATALINA_HOME%\webapps
+cd %CATALINA_HOME%\bin
+%CATALINA_HOME%\bin\startup.bat
