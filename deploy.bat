f536a0a2038567b5519616acdf176bf7388f003d
comp <prokopchukmichail@gmail.com>
2017-09-01 17:03:35 +0300

    refactoring

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
-cd mvn
+cd mvn install
 cd %CATALINA_HOME%\webapps
 DEL %CATALINA_HOME%\webapps\itworkstesttask-rest-1.0-SNAPSHOT.war
 rd %CATALINA_HOME%\webapps\itworkstesttask-rest-1.0-SNAPSHOT /s /q
